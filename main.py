f01377a01d90f06af7131a187851f1c9a6c694ff
kuangfeng <1507173209@qq.com>
2025-03-30 11:50:01 +0800

    测试二

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ class MyPlugin(Star):
     async def my_custom_hook_1(self, event: AstrMessageEvent, req: ProviderRequest): # 请注意有三个参数
         if self.identifier == False:
             return
-        id_Nickname = {'9220528EF8B5534EC8CFA7D96571EBE8':'狂风'}
-        pattern = r"\[User ID: [0-9A-F], Nickname: .*?\]"
-        match = re.search(pattern, req.prompt['contents'][-1]['text'])
+        id_Nickname = {'09DD9BAB91E61DD58E26AEE1848B573A':'狂风'}
+        pattern = r"\[User ID: ([0-9A-F]+), Nickname: .*?\]"
+        match = re.search(pattern, req.prompt['contents'][-1]['parts'][0]['text'])
         if match:
             user_id = match.group(1)
-            if user_id in id_Nickname:
-                nickname = id_Nickname[user_id]
-                req.prompt['contents'][-1]['text'] = req.prompt['contents'][-1]['text'].replace(match.group(0), f"[User ID: {user_id}, Nickname: {nickname}]")
+        if user_id in id_Nickname:
+            nickname = id_Nickname[user_id]
+            req.prompt['contents'][-1]['parts'][0]['text'] = req.prompt['contents'][-1]['parts'][0]['text'].replace(match.group(0), f"[User ID: {user_id}, Nickname: {nickname}]")
